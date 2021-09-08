985134f6c2611a350747ea19a812e576eba81060
Skipzz <77485714+Sk1pzz@users.noreply.github.com>
2021-09-08 10:26:03 +0200

    Update ticket.py

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -12,16 +12,31 @@ class Ticket(commands.Cog):
     async def ticket(self, ctx):
 
         embed = discord.Embed(
-            title="Would you like to create a ticket?", 
-            description="If you have any questions or concerns create a new ticket by clicking on the emoji below this message.", 
-            color=0xf7fcfd)
+            description="Any questions or concerns? We will be happy to assist you.", 
+            color=self.client.color
+        )
 
-        embed.add_field(name="Do you want to report someone?", value="Please contact a supporter or moderator directly!", inline=True)
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/771635939700768769/773121323341578250/external-content.duckduckgo.com.png")
         embed.set_author(name="TiLiKas-Tickets")
+
+        embed.add_field(
+            name="Have you checked if someone already had the problem?",
+            value="To make our work easier, we would be grateful if you first discuss your problem with other members. That takes some work off our shoulders and may also save you some time.",
+            inline=False
+        )
+
+        embed.set_footer(text="NOTICE : one user can only have three tickets at once!")
+        embed.set_thumbnail(url="https://i.imgur.com/OG3IJF9.png")
 
         msg = await ctx.send(embed=embed)
         await msg.add_reaction("📩")
+
+        await self.client.db.execute("UPDATE tickets SET ticket_id = $2 WHERE guild_id = $1", ctx.guild.id, msg.id)
+
+        embed = discord.Embed(color=self.client.color)
+        embed.set_author(name="Successfully created the ticket-system.")
+
+        confirmEmbed = await ctx.send(embed=embed)
+        await confirmEmbed.delete(delay=10)
 
 def setup(client):
     client.add_cog(Ticket(client))
